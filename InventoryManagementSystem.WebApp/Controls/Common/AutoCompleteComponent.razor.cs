454e5093e07503dd7c0f3e5aa52705397cfda713
lusalaMark <lusalamark63@gmail.com>
2023-08-29 19:45:03 +0300

    Reusable Autocomplete component

---
+++ InventoryManagementSystem.WebApp/Controls/Common/AutoCompleteComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryManagementSystem.WebApp.Controls.Common
+{
+    public partial class AutoCompleteComponent : ComponentBase
+    {
+
+    }
+}
---
+++ InventoryManagementSystem.WebApp/Pages/ViewModel/PurchaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagementSystem.WebApp.Pages.ViewModel
+{
+    public class PurchaseViewModel
+    {
+        [Required]
+        public string PONumber { get; set; } = string.Empty;
+        [Required]
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "You have To select an Inventory")]
+        public int InventoryId { get; set; }
+        [Required]
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Qunantity has to be greater than 1")]
+        public int QuantityToPurchase { get; set; }
+        public double InventoryPrice { get; set; }
+    }
+}
