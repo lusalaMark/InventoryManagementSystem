ef758173f8a863add04f8b112c5c93945654f33e
lusalaMark <lusalamark63@gmail.com>
2023-08-29 16:27:41 +0300

    Added product components

---
+++ InventoryManagementSystem.WebApp/Controls/Products/ProductInventoriesComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryManagementSystem.WebApp.Controls.Products
+{
+    public partial class ProductInventoriesComponent : ComponentBase
+    {
+
+    }
+}
