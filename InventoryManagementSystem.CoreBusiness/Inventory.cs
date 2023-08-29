58b14395f86af3464af069a49f058092c87a4ff6
lusalaMark <lusalamark63@gmail.com>
2023-08-29 12:49:46 +0300

    Added Inventory Classes with its column  attributes

---
+++ InventoryManagementSystem.CoreBusiness/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.CoreBusiness
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+        [Required]
+        public string InventoryName { get; set; } = string.Empty;
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity Must Be Greater than Zero")]
+        public int Quantity { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Price Must Be Greater than Zero")]
+        public double Price { get; set; }
+    }
+}
