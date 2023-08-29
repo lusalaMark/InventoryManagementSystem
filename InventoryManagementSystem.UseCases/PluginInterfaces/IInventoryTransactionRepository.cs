9ed649bb32082c9fdf4fe040bf5cb3a0b49e219b
lusalaMark <lusalamark63@gmail.com>
2023-08-29 19:48:19 +0300

    Task impl for the inventory transactions

---
+++ InventoryManagementSystem.UseCases/PluginInterfaces/IInventoryTransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using InventoryManagementSystem.CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.UseCases.PluginInterfaces
+{
+    public interface IInventoryTransactionRepository
+    {
+        Task PurchaseAsync(string? poNumber, Inventory inventory, int quantity, string? doneBy, double price);
+        Task ProduceAsync(string? productionNumber, Inventory inventory, int quantityToConsume, string? doneBy, double price);
+        Task<IEnumerable<InventoryTransaction>> GetInventoryTransactionsAsync(string inventoryName, DateTime? dateFrom, DateTime? dateTo, InventoryTransactionType? transactionType);
+    }
+}
