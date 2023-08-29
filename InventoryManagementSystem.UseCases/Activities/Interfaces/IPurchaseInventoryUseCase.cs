8a96109492250d226f9a22f1f5687f1ece9f3a49
lusalaMark <lusalamark63@gmail.com>
2023-08-29 19:47:12 +0300

    impl activities use cases

---
+++ InventoryManagementSystem.UseCases/Activities/Interfaces/IPurchaseInventoryUseCase.cs
@@ -0,0 +1,14 @@
+﻿using InventoryManagementSystem.CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.UseCases.Activities.Interfaces
+{
+    public interface IPurchaseInventoryUseCase
+    {
+        Task ExecuteAsync(string poNumber, Inventory inventory, int quantity, string doneBy);
+    }
+}
---
+++ InventoryManagementSystem.UseCases/Activities/PurchaseInventoryUseCase.cs
@@ -0,0 +1,35 @@
+﻿using InventoryManagementSystem.CoreBusiness;
+using InventoryManagementSystem.UseCases.Activities.Interfaces;
+using InventoryManagementSystem.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.UseCases.Activities
+{
+    public class PurchaseInventoryUseCase : IPurchaseInventoryUseCase
+    {
+        private readonly IInventoryTransactionRepository inventoryTransactionRepository;
+        private readonly IInventoryRepository inventoryRepository;
+
+        public PurchaseInventoryUseCase(IInventoryTransactionRepository inventoryTransactionRepository,
+            IInventoryRepository inventoryRepository)
+        {
+            this.inventoryTransactionRepository = inventoryTransactionRepository;
+            this.inventoryRepository = inventoryRepository;
+        }
+
+        public async Task ExecuteAsync(string poNumber, Inventory inventory, int quantity, string doneBy)
+        {
+            //insert a record in the transaction table
+            await inventoryTransactionRepository.PurchaseAsync(poNumber, inventory, quantity, doneBy, inventory.Price);
+
+            //increase the quantity
+            inventory.Quantity += quantity;
+            await this.inventoryRepository.UpdateInventoryAsync(inventory);
+        }
+
+    }
+}
