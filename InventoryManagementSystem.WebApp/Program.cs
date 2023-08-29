ac96e3262fb34d0196da2e5ca16fc9a9cfd7bca9
lusalaMark <lusalamark63@gmail.com>
2023-08-29 13:10:28 +0300

    Registered Services

--- InventoryManagementSystem.WebApp/Program.cs
+++ InventoryManagementSystem.WebApp/Program.cs
@@ -1,8 +1,16 @@
+using InventoryManagementSystem.UseCases.Inventories;
+using InventoryManagementSystem.UseCases.Inventories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor(); 
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
+builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
+builder.Services.AddTransient<IEditInventoryUseCase, EditInventoryUseCase>();
+builder.Services.AddTransient<IViewInventoryByIdUseCase, ViewInventoryByIdUseCase>();
 
 var app = builder.Build();
 
